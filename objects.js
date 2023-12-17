525f122fb68fa354a9e86d3120aff9564aa8b043
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-18 00:21:17 +0300

    objects.js

---
+++ objects.js
@@ -0,0 +1,11 @@
+function getVolume() {
+    constructor(radius, height) 
+        let radius = r;
+        let height= h;
+    }
+
+    getVolume() 
+        let volume = Math.PI * Math.pow(r, 2) * h;
+        return volume.toFixed(4);
+    
+console.log(getVolume()); 
