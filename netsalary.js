c0764080b420c5e5b20f0d15a6e46a62c7f59e78
faithchebet <faith.chebet@student.moringaschool.com>
2023-12-18 12:29:50 +0300

    netsalary.js

--- netsalary.js
+++ netsalary.js
@@ -1,19 +1,17 @@
-const KRA_PERCENTAGE = 0.30; 
-const NHIF_PERCENTAGE = 0.05; 
-const NSSF_PERCENTAGE = 0.08; 
+function calculateNetSalary(basicSalary, interest) {
+    const KRA_PERCENTAGE = 0.30;  //kra tax rate//
+    const NHIF_PERCENTAGE = 0.17; //nhif tax rate//
+    const NSSF_PERCENTAGE = 0.06; //nssf tax rate//
 
-function calculateNetSalary(basicSalary, benefits) {
     const tax = basicSalary * KRA_PERCENTAGE;
-
-
     const nhifDeduction = basicSalary * NHIF_PERCENTAGE;
     const nssfDeduction = basicSalary * NSSF_PERCENTAGE;
 
     
-    const grossSalary = basicSalary + benefits;
+    const grossSalary = basicSalary + interest; //calculate gross salary//
 
-    
-    const netSalary = grossSalary - (tax + nhifDeduction + nssfDeduction);
+    const netSalary = grossSalary - (tax + nhifDeduction + nssfDeduction); //calculate net salary//
 
     return netSalary;
 }
+console.log(calculateNetSalary(90000, 11000));
